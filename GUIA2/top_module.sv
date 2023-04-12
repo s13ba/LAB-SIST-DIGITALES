77603b76281de11ab46c2b844d589dd58554aa35
Sebastián Andrade <88354746+s13ba@users.noreply.github.com>
2023-04-12 13:43:01 -0400

    Update top_module.sv

--- GUIA2/top_module.sv
+++ GUIA2/top_module.sv
@@ -26,7 +26,8 @@ FUNCION: modulo que contiene sub-modulos, que permiten tomar la cuenta del count
 
 module top_module(
     input logic clk, reset, ON, //entradas (van al counter) y señal de on.off
-    output logic fib, sevenSeg // salidas (desde elBCD_to_sevenseg y del fibb_rec)
+    output logic fib, 
+    output logic [6:0]sevenSeg // salidas (desde elBCD_to_sevenseg y del fibb_rec)
     );
     //se necesita establecer variables internas que van desde un modulo a otro
     logic [3:0]count; //(salida del contador y entradas a fibb_rec y bcd to 7seg)
@@ -43,14 +44,10 @@ module top_module(
     fib_rec fib_rec( //instaciamos el modulo que toma el numero bcd y lo reconoce como un numerofibbinario
         .BCD_in(count),
         .fib(fib));
-    
+     assign fib =~ fib;.
+     assign sevenSeg =~ sevenSeg;
    // necesitamos invertir 7seg debido a como funciona el display
-    always_comb begin 
-        case(ON)
-        1'b1 : invsevenSeg = ~sevenSeg;
-        1'b1 : invfib = ~fib;
-        1'b0 : sevenSeg = 7'b1111111;               
-    end  
+    
                 
     
     
