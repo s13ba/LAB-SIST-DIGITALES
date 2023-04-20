6a836263eb67244c75446692b92849e5617e56d8
Sebastián Andrade <88354746+s13ba@users.noreply.github.com>
2023-04-19 23:03:12 -0400

    Update ncoun.sv

--- GUIA2/ncoun.sv
+++ GUIA2/ncoun.sv
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 13.04.2023 11:36:50
-// Design Name: 
-// Module Name: ncoun
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module ncoun();
@@ -25,7 +7,7 @@ module ncoun();
     logic clk, reset;
     logic [4:0] counter;
     
-    contadorN #(.N(5)) DUT(
+    contadorN #(.N(3)) DUT(
         .clk(clk),
         .reset(reset),
         .count(counter)
@@ -35,7 +17,7 @@ module ncoun();
     initial begin 
         clk = 1;
         reset = 1;
-        #10 reset =0;
+        #30 reset =0;
         
     end
 endmodule
